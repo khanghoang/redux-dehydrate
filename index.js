8844868b1f937ba4ce7d20731d26bc219cee32a0
Khang Hoang <hoangtrieukhang@gmail.com>
2016-09-18 14:24:19 +0700

    finish first version

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
-export default function () {
-  return () => {
-    return;
-  }
+import { constant } from 'lodash';
+
+export default function (restoreStorePromise) {
+  return (createStore) => (reducer, preloadedState, enhancers) => {
+    if (typeof restoreStorePromise === 'undefined') {
+      return createStore(reducer, preloadedState, enhancers);
+    }
+
+    const state = {};
+    const fakeReducer = constant(state);
+    const store = createStore(fakeReducer, state, enhancers);
+
+    let resolved = false;
+    const actionsQueue = [];
+
+    const dispatch = (...action) => {
+      if (!resolved) {
+        actionsQueue.push(action);
+        return;
+      }
+
+      store.dispatch(action);
+    };
+    
+    restoreStorePromise
+      .then(restoredState => {
+        Object.assign(state, restoredState);
+      })
+      .catch(() => {
+        Object.assign(state, preloadedState);
+      })
+      .then(() => {
+        resolved = true;
+        store.replaceReducer(reducer);
+
+        actionsQueue.forEach(action => {
+          store.dispatch(...action);
+        });
+      })
+
+    return {
+      ...store,
+      dispatch,
+    };
+  };
 }
--- index.test.js
+++ index.test.js
@@ -1,8 +1,54 @@
 import { expect } from 'chai';
+import { createStore, compose } from 'redux';
+import { constant, identity } from 'lodash';
 import restoreReduxStore from './index';
 
 describe('Restore Redux Store enhancer', () => {
   it('should return function', () => {
     expect(restoreReduxStore()).to.be.a('function');
+  });
+
+  it('should return Store', () => {
+    const store = restoreReduxStore()(createStore)(constant({}), {});
+    const { dispatch, subscribe, getState, replaceReducer } = store;
+
+    /* eslint-disable */
+    expect(dispatch).to.be.not.undefined;
+    expect(subscribe).to.be.not.undefined;
+    expect(getState).to.be.not.undefined;
+    expect(replaceReducer).to.be.not.undefined;
+    /* eslint-enable */
+  });
+
+  it('the store will have initial state from promise', (done) => {
+    const restorePromise = () => new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ foo: 'bar' });
+      }, 0);
+    });
+
+    const store = createStore(identity, { foo: '' }, compose(restoreReduxStore(restorePromise())));
+
+    store.subscribe(() => {
+      const currentState = store.getState();
+      expect(currentState.foo).to.equal('bar');
+      done();
+    });
+  });
+
+  it('should return initial state when promise fails', (done) => {
+    const restorePromise = () => new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject(new Error('foo error'));
+      }, 0);
+    });
+
+    const store = createStore(identity, { foo: '' }, compose(restoreReduxStore(restorePromise())));
+
+    store.subscribe(() => {
+      const currentState = store.getState();
+      expect(currentState.foo).to.equal('');
+      done();
+    });
   });
 });
